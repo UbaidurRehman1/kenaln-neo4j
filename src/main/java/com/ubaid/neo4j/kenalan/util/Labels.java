3e8d7db5e8de36417fa3b5c26397390bcf0afefa
UbaidurRehman1 <ubaidshan007@gmail.com>
2019-09-29 22:47:38 +0500

    label changed

--- src/main/java/com/ubaid/neo4j/kenalan/util/Labels.java
+++ src/main/java/com/ubaid/neo4j/kenalan/util/Labels.java
@@ -1,9 +1,9 @@
 package com.ubaid.neo4j.kenalan.util;
 
 public class Labels {
-    public static final String IS_ASSIGNED = "IS_ASSIGNED";
-    public static final String IS = "IS";
-    public static final String IS_DOING_MAINTENANCE_EVENT = "IS_DOING_MAINTENANCE_EVENT";
+    public static final String IS_ASSIGNED = "IS_AGENT_INCLUDED_IN";
+    public static final String IS = "IS_MEMBER_OF";
+    public static final String IS_DOING_MAINTENANCE_EVENT = "IS_OBJECT_INCLUDED_IN";
     public static final String IS_MADE_BY = "IS_MADE_BY";
     public static final String IS_MADE_OF = "IS_MADE_OF";
 }
